230efef906cbbd0cefd55a1ab4e72a34748d0935
Hawker <nyabentley@gmail.com>
2024-11-03 17:11:59 -0500

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,24 @@
+require('dotenv').config()
+const { Client, IntentsBitField} = require('discord.js')
+
+const client = new Client({
+    intents: [
+        IntentsBitField.Flags.Guilds,
+        IntentsBitField.Flags.GuildMembers,
+        IntentsBitField.Flags.GuildMessages,
+        IntentsBitField.Flags.MessageContent,
+    ],
+});
+
+client.on('ready', (c) => {
+    console.log(`${c.user.tag} is online`);
+})
+
+client.on('messageCreate', (message) => {
+    if(message.author.bot) {
+        return;
+    }
+
+    message.channel.send(message.content);
+})
+client.login(process.en)
