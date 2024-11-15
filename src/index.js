272ca8c70d78885e20c1209fa8ea9a351b3633cf
Hawker <62062830+BlckHawker@users.noreply.github.com>
2024-11-15 16:46:59 -0500

    Remove role from "allowedRoles" when deleted from servers

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
-const { saveToDataFile, readDataFile, replaceLink, createNewObject } = require("./utils");
-const { registerCommands, handleCommand } = require("./commands");
+const utils = require("./utils");
+const commands = require("./commands");
+const apiCalls = require('./apiCalls')
 const { Client, IntentsBitField } = require("discord.js");
 const client = new Client({
   intents: [IntentsBitField.Flags.Guilds, IntentsBitField.Flags.GuildMembers, IntentsBitField.Flags.GuildMessages, IntentsBitField.Flags.MessageContent],
@@ -17,13 +18,13 @@ client.on("messageCreate", async (message) => {
    * the bot is disabled in the sever */
 
   const serverId = message.channel.guild.id;
-  let data = readDataFile();
+  let data = utils.readDataFile();
   const serverObj = data.find((obj) => obj.serverId === serverId);
   if (message.author.bot || !message.content || !serverObj.enabled) {
     return;
   }
 
-  const found = replaceLink(message.content)
+  const found = utils.replaceLink(message.content)
   if (found === undefined) {
     return;
   }
@@ -42,25 +43,40 @@ client.on("messageCreate", async (message) => {
 client.on("guildCreate", (guild) => {
   //When the bot joins a server,
   //create a new object with the server id saved and "enabled" being set to false
-  let data = readDataFile();
-  data.push(createNewObject(guild.id));
-  saveToDataFile(data);
+  let data = utils.readDataFile();
+  data.push(utils.createNewObject(guild.id));
+  utils.saveToDataFile(data);
 
   //register commands
-  registerCommands(guild.id);
+  commands.registerCommands(guild.id);
 });
 
 client.on("guildDelete", (guild) => {
   //when the bot leaves the server, remove from data json
-  let data = readDataFile();
+  let data = utils.readDataFile();
   data = data.filter((obj) => obj.serverId !== guild.id);
-  saveToDataFile(data);
+  utils.saveToDataFile(data);
 });
+
+client.on("roleDelete", (role) => {
+  //when the a role is removed from a server, verify that it's not in "allowedRoles" for that server
+  const roleId = role.id;
+  const serverId = role.guild.id;
+  let data = utils.readDataFile();
+  const serverObj = data.find((obj) => obj.serverId === serverId);
+  data = data.filter((obj) => obj.serverId !== serverId);
+  if(serverObj.allowedRoles.includes(roleId)) {
+    const newRoles = serverObj.allowedRoles.filter(id => id !== roleId);
+    const newObj = utils.updateObject(serverObj, 1, newRoles);
+    data.push(newObj);
+    utils.saveToDataFile(data);
+  }
+})
 
 client.on("interactionCreate", async (interaction) => {
   if (!interaction.isChatInputCommand()) return;
 
-  await handleCommand(interaction);
+  await commands.handleCommand(interaction);
 });
 
 client.login(process.env.DISCORD_TOKEN);
